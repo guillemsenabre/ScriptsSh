271725cc5c019e919c0ccb861c4a0429c626497f
Guillem Senabre i Prades <guillemsenabre@gmail.com>
2024-07-23 16:17:55 +0200

    improvements on check_commits script

--- commits/check_commits.sh
+++ commits/check_commits.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: Consider exporting the current commit as an environment variable to continue from that commit next time
 # cd into kernel directory
 pushd ~/kernel_last
 
@@ -20,11 +21,14 @@ do
     echo "Commit: $current_details"
     echo "--------------------------------------------------"
 
+    # This input is used to be able to read the current_details variable before showing details.
+    read -p "Press [Enter] to show details of the commit"
+
     # Show the details of the commit using less for pagination
     git show "$current_commit" | less -R
 
     # Wait for user input before moving to the next commit
-    read -p "Press [Enter] to continue to the next commit..."
+    #read -p "Press [Enter] to continue to the next commit..." # Consider removing: not necessary if read above is active
 done
 
 popd
